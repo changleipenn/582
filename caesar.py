ab852e4a9eda6c440cefaa823d04b9fdfedcabf1
changlei.li <changlei.li@seas.upenn.edu>
2022-01-13 20:50:45 -0500

    updated code

--- caesar.py
+++ caesar.py
@@ -1,12 +1,16 @@
 
 def encrypt(key,plaintext):
     ciphertext=""
-    #YOUR CODE HERE
+    #YOUR CODE here
+    ciphertext =''.join([chr(((ord(x)-65)+key)%26+65) for x in plaintext])
     return ciphertext
 
 def decrypt(key,ciphertext):
     plaintext=""
     #YOUR CODE HERE
+    plaintext =''.join([chr(((ord(x)-65)-key)%26+65) for x in ciphertext])
     return plaintext
 
 
+# print(encrypt(1,"ABCD"))
+# print(decrypt(-1,"BCDE"))
