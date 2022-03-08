3666291cc3227f2ab3fcec7861e7a8e345e6a505
changlei.li <changlei.li@seas.upenn.edu>
2022-03-08 17:13:52 -0500

    sendtoken2

--- send_tokens.py
+++ send_tokens.py
@@ -51,20 +51,20 @@ def send_tokens( receiver_pk, tx_amount ):
     txid = acl.send_transaction(signed_txn)
     print("Successfully sent transaction with txID: {}".format(txid))
 
-    # wait for confirmation 
-    try:
-        confirmed_txn = transaction.wait_for_confirmation(algod_client, txid, 4)  
-    except Exception as err:
-        print(err)
-        return
+    # # wait for confirmation 
+    # try:
+    #     confirmed_txn = transaction.wait_for_confirmation(algod_client, txid, 4)  
+    # except Exception as err:
+    #     print(err)
+    #     return
 
-    print("Transaction information: {}".format(
-        json.dumps(confirmed_txn, indent=4)))
-    print("Decoded note: {}".format(base64.b64decode(
-        confirmed_txn["txn"]["txn"]["note"]).decode()))
-    print("Starting Account balance: {} microAlgos".format(account_info.get('amount')) )
-    print("Amount transfered: {} microAlgos".format(amount) )    
-    print("Fee: {} microAlgos".format(params.fee) ) 
+    # print("Transaction information: {}".format(
+    #     json.dumps(confirmed_txn, indent=4)))
+    # print("Decoded note: {}".format(base64.b64decode(
+    #     confirmed_txn["txn"]["txn"]["note"]).decode()))
+    # print("Starting Account balance: {} microAlgos".format(account_info.get('amount')) )
+    # print("Amount transfered: {} microAlgos".format(amount) )    
+    # print("Fee: {} microAlgos".format(params.fee) ) 
 
 
     account_info = algod_client.account_info(my_address)
