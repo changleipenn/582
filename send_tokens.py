35bc470d37d04601feab3fc796c4a2645339a260
changlei.li <changlei.li@seas.upenn.edu>
2022-03-08 17:10:26 -0500

    sendtoken

---
+++ send_tokens.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+
+from algosdk.v2client import algod
+from algosdk import mnemonic
+from algosdk import transaction
+from algosdk import constants
+from algosdk import account, encoding
+
+#Connect to Algorand node maintained by PureStake
+algod_address = "https://testnet-algorand.api.purestake.io/ps2"
+algod_token = "B3SU4KcVKi94Jap2VXkK83xx38bsv95K5UZm2lab"
+#algod_token = 'IwMysN3FSZ8zGVaQnoUIJ9RXolbQ5nRY62JRqF2H'
+headers = {
+   "X-API-Key": algod_token,
+}
+
+acl = algod.AlgodClient(algod_token, algod_address, headers)
+min_balance = 100000 #https://developer.algorand.org/docs/features/accounts/#minimum-balance
+
+
+# generate an account
+private_key,address = "+BGLUlW6kQ1Bl77GxOPc4fgakcEU4/4oueynIDpQfGfj5JAyhmRn+B/bge1TcsXe4naOEMssSOZMgH4mbf8iHw==","4PSJAMUGMRT7QH63QHWVG4WF33RHNDQQZMWERZSMQB7CM3P7EIP52WTYUM"
+#private_key, address = account.generate_account()
+print("Private key:", private_key)
+print("Address:", address)
+
+
+def send_tokens( receiver_pk, tx_amount ):
+    params = acl.suggested_params()
+    gen_hash = params.gh
+    first_valid_round = params.first
+    tx_fee = params.min_fee
+    last_valid_round = params.last
+
+    #Your code here
+    ########################################################
+    ##https://developer.algorand.org/docs/sdks/python/#install-sandbox
+    params = acl.suggested_params()
+    # comment out the next two (2) lines to use suggested fees
+    params.flat_fee = True
+    params.fee = constants.MIN_TXN_FEE 
+
+    unsigned_txn = transaction.PaymentTxn(address, params.fee, params.first, params.last, params.gh, receiver_pk, tx_amount, flat_fee=True)
+    #unsigned_txn = transaction.PaymentTxn(address, params, receiver, amount, None, note)
+
+    #sign transaction
+    signed_txn = unsigned_txn.sign(private_key)
+
+
+    #submit transaction
+    txid = acl.send_transaction(signed_txn)
+    print("Successfully sent transaction with txID: {}".format(txid))
+
+    # wait for confirmation 
+    try:
+        confirmed_txn = transaction.wait_for_confirmation(algod_client, txid, 4)  
+    except Exception as err:
+        print(err)
+        return
+
+    print("Transaction information: {}".format(
+        json.dumps(confirmed_txn, indent=4)))
+    print("Decoded note: {}".format(base64.b64decode(
+        confirmed_txn["txn"]["txn"]["note"]).decode()))
+    print("Starting Account balance: {} microAlgos".format(account_info.get('amount')) )
+    print("Amount transfered: {} microAlgos".format(amount) )    
+    print("Fee: {} microAlgos".format(params.fee) ) 
+
+
+    account_info = algod_client.account_info(my_address)
+    print("Final Account balance: {} microAlgos".format(account_info.get('amount')) + "\n")
+
+    return sender_pk, txid
+
+# Function from Algorand Inc.
+def wait_for_confirmation(client, txid):
+    """
+    Utility function to wait until the transaction is
+    confirmed before proceeding.
+    """
+    last_round = client.status().get('last-round')
+    txinfo = client.pending_transaction_info(txid)
+    while not (txinfo.get('confirmed-round') and txinfo.get('confirmed-round') > 0):
+        print("Waiting for confirmation")
+        last_round += 1
+        client.status_after_block(last_round)
+        txinfo = client.pending_transaction_info(txid)
+    print("Transaction {} confirmed in round {}.".format(txid, txinfo.get('confirmed-round')))
+    return txinfo
+
+
+
+
+
+
+#send_tokens("HZ57J3K46JIJXILONBBZOHX6BKPXEM2VVXNRFSUED6DKFD5ZD24PMJ3MVA",1000000)
