9fa1707ca76c4dab0faf4254d3bc7b1467263ce2
changlei.li <changlei.li@seas.upenn.edu>
2022-03-09 22:20:01 -0500

    verify

---
+++ verification_endpoint.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+from flask_restful import Api
+import json
+import eth_account
+import algosdk
+
+app = Flask(__name__)
+api = Api(app)
+app.url_map.strict_slashes = False
+
+@app.route('/verify', methods=['GET','POST'])
+def verify():
+    content = request.get_json(silent=True)
+
+    #Check if signature is valid
+    sig = content['sig']
+    message = content['payload']['message']
+    pk = content['payload']['pk']
+    platform = content['payload']['platform']
+
+    if platform == "Ethereum":
+        result = eth_account.Account.recover_message(message,sig.hex()) == pk
+    else:
+        result = algosdk.util.verify_bytes(message.encode('utf-8'),sig,pk)
+
+    result = True #Should only be true if signature validates
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(port='5002')
